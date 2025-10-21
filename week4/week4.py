f65162c6904726ffeabd10628b92259cd98f7bac
gis-14187504 <yi.luo-5@postgrad.manchester.ac.uk>
2025-10-21 13:44:07 +0100

    remove

--- week4/week4.py
+++ week4/week4.py
@@ -1,4 +1,20 @@
 from geopandas import read_file
+from math import sqrt
+
+def distance(x1,y1,x2,y2):
+    return sqrt((x2-x1)**2+(y2-y1)**2)
+
+def get_effective_area(a, b, c):
+	# calculate the length of each side
+	side_a = distance(b[0], b[1], c[0], c[1])
+	side_b = distance(a[0], a[1], c[0], c[1])
+	side_c = distance(a[0], a[1], b[0], b[1])
+
+	# calculate semi-perimeter of the triangle (perimeter / 2)
+	s = (side_a + side_b + side_c) / 2
+
+	# apply Heron's formula and return
+	return sqrt(s * (s - side_a) * (s - side_b) * (s - side_c))
 
 # open a dataset of all countries in the world
 world = read_file("P:/UGIS/understandinggis/data/natural-earth/ne_10m_admin_0_countries.shp")
@@ -29,7 +45,7 @@ for poly in uk.geoms:
      # store the coordinates of the polygon
 		coord_list = list(poly.boundary.coords)	# COMPLETE THIS LINE (look at the variables that you defined before the loop)
         
-        # set the percentage of nodes that you want to remove
+# set the percentage of nodes that you want to remove
 SIMPLIFICATION_PERC = 98
 
 # how many nodes do we need?
@@ -37,4 +53,15 @@ n_nodes = int(len(coord_list) / 100.0 * (100 - SIMPLIFICATION_PERC))
 
 # ensure that there are at least 3 nodes (minimum for a polygon)
 if n_nodes < 3:
-	n_nodes = 3 
+    odes = 3 
+
+# loop through each node, excluding the end points
+areas = []
+for i in range(1, len(coord_list)-1):
+
+  # get the effective area
+  area = get_effective_area(coord_list[i-1], coord_list[i], coord_list[i+1] )	# COMPLETE THIS LINE
+
+  # append the node and effective area to the list
+  areas.append({"point": coord_list[i], "area": area})
+  print(areas)
