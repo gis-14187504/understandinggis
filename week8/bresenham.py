bf538f84ead20338469fea40236c0d8bbffeecf7
gis-14187504 <yi.luo-5@postgrad.manchester.ac.uk>
2025-11-18 15:17:20 +0000

    print
    print

--- week8/bresenham.py
+++ week8/bresenham.py
@@ -3,6 +3,7 @@ from rasterio import open as rio_open
 from rasterio.plot import show as rio_show
 from matplotlib.pyplot import subplots, savefig
 from matplotlib.colors import LinearSegmentedColormap
+from rasterio.transform import rowcol, xy
 
 # open the elevation data file
 with rio_open("../data/helvellyn/Helvellyn-50.tif") as dem:
@@ -29,5 +30,23 @@ rio_show(
     transform=dem.transform,
     cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.5)], N=2)
     )
-savefig('./out/bresenham.png', bbox_inches='tight')# -*- coding: utf-8 -*-# -*- coding: utf-8 -*-
+savefig('./out/bresenham.png', bbox_inches='tight')# -*- coding: utf-8 -*-
 
+def coord_2_img(transform, x, y):
+    """convert coordinate space (x,y) → image space (row,col)."""
+    r, c = rowcol(transform, x, y)
+    return int(r), int(c)
+
+x = 334170
+y = 515165
+
+row, col = coord_2_img(dem.transform, x, y)
+print("row, col =", row, col)
+
+output[row-1:row+2, col-1:col+2] = 1
+
+red_cmap = LinearSegmentedColormap.from_list(
+    'red_point',
+    [(0, 0, 0, 0), (1, 0, 0, 1)],
+    N=2
+)
