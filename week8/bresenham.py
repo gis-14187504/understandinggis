4d63055509dea1397439d1fd3560dad21dd840ce
gis-14187504 <yi.luo-5@postgrad.manchester.ac.uk>
2025-11-24 17:40:46 +0000

    Drawing your map
    Drawing your map

--- week8/bresenham.py
+++ week8/bresenham.py
@@ -1,13 +1,19 @@
-from numpy import zeros
+from numpy import zeros, column_stack
 from rasterio import open as rio_open
 from rasterio.plot import show as rio_show
-from matplotlib.pyplot import subplots, savefig
-from matplotlib.colors import LinearSegmentedColormap
-from rasterio.transform import rowcol, xy
-from skimage.draw import line
-from numpy import column_stack
-from skimage.draw import circle_perimeter
+from rasterio.transform import rowcol
+from skimage.draw import line, circle_perimeter
 from sys import exit
+from math import hypot, floor, ceil
+from geopandas import GeoSeries
+from shapely.geometry import Point
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
+from matplotlib.colors import Normalize, LinearSegmentedColormap
+from matplotlib.cm import ScalarMappable
+from matplotlib.pyplot import subplots, savefig
+from matplotlib_scalebar.scalebar import ScaleBar
+
 
 # open the elevation data file
 with rio_open("../data/helvellyn/Helvellyn-50.tif") as dem:
@@ -21,8 +27,6 @@ with rio_open("../data/helvellyn/Helvellyn-50.tif") as dem:
 def coord_2_img(transform, x, y):
     r, c = rowcol(transform, x, y)
     return (int(r), int(c))
-
-from math import hypot
 
 def line_of_sight(r0, c0, height0,
                   r1, c1, height1,
@@ -101,6 +105,9 @@ row, col = coord_2_img(dem.transform, x, y)
 # print out the elevation at that location by reading it from the dataset
 print(f"{dem_data[row][col]:.0f}m")	# note that this makes use of an f-string to format the number
 
+# calculate the viewshed
+output = viewshed(x0, y0, 20000, 1.8, 100, dem_data, dem.transform)
+
 print(column_stack(line(row, col, row, col+50)))
 
 output[row, col] = 1
@@ -109,34 +116,66 @@ output[line(row, col, row, col+50)] = 1
 
 output[circle_perimeter(row, col, 50)] = 1
 
-# calculate the viewshed
-output = viewshed(x0, y0, 20000, 1.8, 100, dem_data, dem.transform)
-
-
-
-
-
-# plot the dataset
+# output image
 fig, my_ax = subplots(1, 1, figsize=(16, 10))
+my_ax.set_title("Viewshed Analysis")
 
-# add the DEM
+	# draw dem
 rio_show(
-  dem_data,
-  ax=my_ax,
-  transform = dem.transform,
-)
-# add the drawing layer
+	dem_data,
+	ax=my_ax,
+	transform = dem.transform,
+	cmap = 'viridis',
+	)
+
+	# draw dem as contours
 rio_show(
-    output,
-    ax=my_ax,
-    transform=dem.transform,
-    cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.8)], N=2)
-    )
+	dem_data,
+	ax=my_ax,
+	contour=True,
+	transform = dem.transform,
+	colors = ['white'],
+	linewidths = [0.5],
+	)
 
+# add viewshed
+rio_show(
+	output,
+	ax=my_ax,
+	transform=dem.transform,
+	cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.5)], N=2)
+	)
 
-savefig('./out/bresenham.png', bbox_inches='tight')# -*- coding: utf-8 -*-
+# add origin point
+GeoSeries(Point(x0, y0)).plot(
+	ax = my_ax,
+	markersize = 60,
+	color = 'black',
+	edgecolor = 'white'
+	)
 
+# add a colour bar
+fig.colorbar(ScalarMappable(norm=Normalize(vmin=floor(dem_data.min()), vmax=ceil(dem_data.max())), cmap='viridis'), ax=my_ax, pad=0.01)
 
+# add north arrow
+x, y, arrow_length = 0.97, 0.99, 0.1
+my_ax.annotate('N', xy=(x, y), xytext=(x, y-arrow_length),
+	arrowprops=dict(facecolor='black', width=5, headwidth=15),
+	ha='center', va='center', fontsize=20, xycoords=my_ax.transAxes)
+
+# add scalebar
+my_ax.add_artist(ScaleBar(dx=1, units="m", location="lower right"))
+
+# add legend for point
+my_ax.legend(
+	handles=[
+		Patch(facecolor=(1, 0, 0, 0.5), edgecolor=None, label=f'Visible Area'),
+		Line2D([0], [0], marker='o', color=(1,1,1,0), label='Viewshed Origin', markerfacecolor='black', markersize=8)
+	], loc='lower left')
+
+# save the result	
+savefig('./out/7.png', bbox_inches='tight')
+print("done!")
 
 
 
