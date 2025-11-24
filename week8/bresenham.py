14e0a90d88d33e29b788f7eeebba636f39d8d478
gis-14187504 <yi.luo-5@postgrad.manchester.ac.uk>
2025-11-24 16:39:28 +0000

    Drawing a Line
    Drawing a Line

--- week8/bresenham.py
+++ week8/bresenham.py
@@ -4,6 +4,8 @@ from rasterio.plot import show as rio_show
 from matplotlib.pyplot import subplots, savefig
 from matplotlib.colors import LinearSegmentedColormap
 from rasterio.transform import rowcol, xy
+from skimage.draw import line
+from numpy import column_stack
 
 # open the elevation data file
 with rio_open("../data/helvellyn/Helvellyn-50.tif") as dem:
@@ -13,6 +15,23 @@ with rio_open("../data/helvellyn/Helvellyn-50.tif") as dem:
 
     # create a new 'band' of raster data the same size
     output = zeros(dem_data.shape, dtype='uint8')
+
+def coord_2_img(transform, x, y):
+    r, c = rowcol(transform, x, y)
+    return (int(r), int(c))
+
+x = 334170
+y = 515165
+
+# transform the coordinates for the summit of Helvellyn into image space
+row, col = coord_2_img(dem.transform, x, y)
+
+# print out the elevation at that location by reading it from the dataset
+print(f"{dem_data[row][col]:.0f}m")	# note that this makes use of an f-string to format the number
+
+print(column_stack(line(row, col, row, col+50)))
+
+output[row, col] = 1
 
 # plot the dataset
 fig, my_ax = subplots(1, 1, figsize=(16, 10))
@@ -28,25 +47,14 @@ rio_show(
     output,
     ax=my_ax,
     transform=dem.transform,
-    cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.5)], N=2)
+    cmap = LinearSegmentedColormap.from_list('binary_viewshed', [(0, 0, 0, 0), (1, 0, 0, 0.8)], N=2)
     )
+
+
+
 savefig('./out/bresenham.png', bbox_inches='tight')# -*- coding: utf-8 -*-
 
-def coord_2_img(transform, x, y):
-    """convert coordinate space (x,y) → image space (row,col)."""
-    r, c = rowcol(transform, x, y)
-    return int(r), int(c)
 
-x = 334170
-y = 515165
 
-row, col = coord_2_img(dem.transform, x, y)
-print("row, col =", row, col)
 
-output[row-1:row+2, col-1:col+2] = 1
 
-red_cmap = LinearSegmentedColormap.from_list(
-    'red_point',
-    [(0, 0, 0, 0), (1, 0, 0, 1)],
-    N=2
-)
